223b4c8c49bc7a5e9360f699532633fa30c23f3f
Yurii B <drammer.g@gmail.com>
2025-06-04 12:34:32 +0300

    Add react next

---
+++ pages/welcome.tsx
@@ -0,0 +1,11 @@
+// pages/welcome.tsx
+import React from 'react';
+
+export default function Welcome() {
+  return (
+    <main>
+      <h1>Welcome Page</h1>
+      <p>This is the welcome page!</p>
+    </main>
+  );
+}
